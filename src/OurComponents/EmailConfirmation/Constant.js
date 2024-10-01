0adae2e0c115856c505a5c5f7fb88dd8d67211e5
shivam2534 <134926520+shivamkanchole@users.noreply.github.com>
2024-10-01 13:03:09 +0530

    change backend link

--- src/OurComponents/EmailConfirmation/Constant.js
+++ src/OurComponents/EmailConfirmation/Constant.js
@@ -1,4 +1,6 @@
 // export const DeployedBackendURL = "https://subspot-backend.onrender.com";
 export const DeployedBackendURL = "https://subspot-backend-1.onrender.com";
+
+
 // export const DeployedBackendURL = "http://localhost:3000";
 
